fe249be655cc75aca1fb71053ee1ea846bef02b1
ClauHaus <claudio.aime32@gmail.com>
2021-09-25 10:11:22 +0200

    Update index.css

--- src/timeZones/UTC1.js
+++ src/timeZones/UTC1.js
@@ -63,7 +63,7 @@ const UTC1 = () => {
       setTitle("title-early-night");
       setSubtitle("subtitle-early-night");
     }
-    if (hour >= 22 && hour < 7) {
+    if (hour >= 22 || hour < 7) {
       setTheme("theme-night");
       setTitle("title-night");
       setSubtitle("subtitle-night");
